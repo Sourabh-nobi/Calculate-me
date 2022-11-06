591048b513cc665b72261da6211efaf73a53b41d
Sourabh-nobi <95531655+Sourabh-nobi@users.noreply.github.com>
2022-11-06 14:08:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+
+
+        else {
+
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
+
+function backRemovel() {
+    let txt = document.getElementById("input").value; am = txt.slice(0, -1); document.getElementById("input").value = am;
+}
